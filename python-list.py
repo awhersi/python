0aa9b53f8d09b6d70c81c2bb3126b653b5a6d23b
Ahmed Hersi <116831335+awhersi@users.noreply.github.com>
2022-12-25 13:21:06 -0500

    Create python-list.py
    working with lists

---
+++ python-list.py
@@ -0,0 +1,26 @@
+#create empty list
+aws_list = []
+
+#insert items into list
+aws_list.append("Amazon EC2")
+aws_list.append("Amazon RDS")
+aws_list.append("Amazon S3")
+aws_list.append("Amazon IAM")
+aws_list.append("Amazon EBS")
+aws_list.append("Amazon Lambda")
+aws_list.append("Amazon EFS")
+aws_list.append("Amazon CloudFront")
+aws_list.append("Amazon SNS")
+aws_list.append("Amazon VPC")
+
+#print list and length of list
+print(aws_list)
+print(f"There are {len(aws_list)} items in this list")
+
+#delete items from list
+del aws_list[3]
+del aws_list[7]
+
+#print updated list and length of list
+print(aws_list)
+print(f"There are {len(aws_list)} items in this list")
