276e23cf371121633d8109d32f10622b0c6bfe90
Ahmed Hersi <116831335+awhersi@users.noreply.github.com>
2023-01-03 21:20:49 -0500

    created fizz-buzz with user input
    created fizz-buzz with user input

---
+++ fizzbuzz_user_input.py
@@ -0,0 +1,10 @@
+user_number = int(input("Enter a number: "))
+for number in range(0, user_number + 1):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
