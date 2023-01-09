c0837f26f32a6c743a04f5fa39afa967c6889c48
Ahmed Hersi <116831335+awhersi@users.noreply.github.com>
2023-01-08 21:52:38 -0500

    Update EC2_Random_Name_Generator.py
    updated to allow for error handling of non-whole number characters and numbers less than zero

--- EC2_Random_Name_Generator.py
+++ EC2_Random_Name_Generator.py
@@ -16,12 +16,15 @@ def check_dept(num_instances, dept_name):
         dept_name += '_'
         name_generator(num_instances, dept_name)
     else:
-        print("Sorry, this Name Generator can only be used by the Marketing, Accounting, and FinOps Departments")
+        print("Sorry, this Name Generator can only be used by the Marketing, Accounting, and FinOps Departments.")
 
 
 while True:
-    num = int(input("Number of instances to name: "))
-    if num > 0:
+    num = input("How many instances would you like to name: ")
+    if num.isdigit() and int(num) > 0:
         break
-dept = input("Department (Marketing, Accounting, or FinOps): ").lower()
-check_dept(num, dept)
+    else:
+        print(f"\n'{num}' is not a valid entry, please enter a whole number greater than 0.")
+dept = input("\nEnter your Department (Marketing, Accounting, or FinOps): ").lower()
+print('')
+check_dept(int(num), dept)
