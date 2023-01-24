144af00c7aadab6dd3472d82184f6a1efc84b65a
Ahmed Hersi <116831335+awhersi@users.noreply.github.com>
2023-01-24 15:42:42 -0500

    updated create_sqs

--- boto3/sqs/create_sqs_boto3.py
+++ boto3/sqs/create_sqs_boto3.py
@@ -2,18 +2,15 @@ import logging
 import boto3
 from botocore.exceptions import ClientError
 
-
 # delcaring the name of the queue and the region it will be created in
 queue_name = 'my_sqs_queue'
 region = 'us-east-1'
-
 
 # logger config
 logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO, 
 format='%(asctime)s: %(levelname)s: %(message)s')
 sqs_resource = boto3.resource("sqs", region_name=region)
-
 
 def create_queue(queue_name, ds, vt):
     #Creating a standard SQS queue
@@ -24,11 +21,10 @@ def create_queue(queue_name, ds, vt):
                                                  'VisibilityTimeout': vt
                                              })
     except ClientError:
-        logger.exception(f'Could not create - {queue_name}.')
+        logger.exception(f'Could not create: {queue_name}.')
         raise
     else:
         return response
-        
 
 if __name__ == '__main__':
     # CONSTANTS
@@ -37,4 +33,4 @@ if __name__ == '__main__':
     vt = '60'
     output = create_queue(queue_name, ds, vt)
     logger.info(
-        f'{queue_name} created. Queue URL - {output.url}')
+        f'{queue_name} created. Queue URL: {output.url}')
